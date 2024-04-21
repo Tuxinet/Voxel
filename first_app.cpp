02c8e73addf729e8cc5baeb365ec07629d78b103
Trym Kristiansen <trym@trymk.no>
2024-04-21 19:18:40 +0200

    Use index buffer for loaded models

--- first_app.cpp
+++ first_app.cpp
@@ -65,69 +65,23 @@ void FirstApp::run() {
   vkDeviceWaitIdle(lveDevice.device());
 }
 
-std::unique_ptr<LveModel> createCubeModel(LveDevice &device, glm::vec3 offset) {
-  LveModel::Builder modelBuilder{};
-  modelBuilder.vertices = {
-      // left face (white)
-      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-      {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
-      {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
-
-      // right face (yellow)
-      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-      {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
-      {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
-
-      // top face (orange, remember y axis points down)
-      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-      {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-      {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-
-      // bottom face (red)
-      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-      {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
-      {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-
-      // nose face (blue)
-      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-      {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-      {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-
-      // tail face (green)
-      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-      {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-      {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-  };
-  for (auto &v : modelBuilder.vertices) {
-    v.position += offset;
-  }
-
-  modelBuilder.indices = {0,  1,  2,  0,  3,  1,  4,  5,  6,  4,  7,  5,
-                          8,  9,  10, 8,  11, 9,  12, 13, 14, 12, 15, 13,
-                          16, 17, 18, 16, 19, 17, 20, 21, 22, 20, 23, 21};
-
-  return std::make_unique<LveModel>(device, modelBuilder);
-}
-
 void FirstApp::loadGameObjects() {
   std::shared_ptr<LveModel> lveModel =
-      createCubeModel(lveDevice, {.0f, .0f, .0f});
+      LveModel::createModelFromFile(lveDevice, "models/smooth_vase.obj");
 
-  for (int x = 0; x < 10; x++) {
-    for (int y = 0; y < 100; y++) {
-      auto cube = LveGameObject::createGameObject();
-      cube.model = lveModel;
-      cube.transform.translation = {-2.5f + x * 0.5f, 1.5f, 2.5f + y * 0.5f};
-      cube.transform.scale = {.5f, .5f, .5f};
+  for (int x = 0; x < 20; x++) {
+    for (int y = 0; y < 20; y++) {
+      for (int z = 0; z < 20; z++) {
+        auto cube = LveGameObject::createGameObject();
+        cube.model = lveModel;
+        cube.transform.translation = {-2.5f + x * 1.2f, 1.5f + y * 1.2f, 2.5f + z * 1.2f};
+        cube.transform.scale = {.5f, .5f, .5f};
 
-      gameObjects.push_back(std::move(cube));
+        gameObjects.push_back(std::move(cube));
+      }
     }
   }
+
+  std::cout << "Number of gameobjects: " << gameObjects.size() << std::endl;
 }
 } // namespace lve
